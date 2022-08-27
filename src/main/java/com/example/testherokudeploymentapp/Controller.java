8d9c11792e7fb1139571012b573a38394c3094aa
decagon <phabripass@yahoo.com>
2022-08-27 11:16:17 +0100

    added world

--- src/main/java/com/example/testherokudeploymentapp/Controller.java
+++ src/main/java/com/example/testherokudeploymentapp/Controller.java
@@ -22,6 +22,6 @@ public class Controller {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Thank God it worked ohhhhh!!!!";
+        return "Thank God it worked oohhhhh!!!!";
     }
 }
